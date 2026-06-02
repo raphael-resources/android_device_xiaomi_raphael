9a01994269682724c29c630ed84eed173888c956
NoCache-69 <chethangowdasj86@gmail.com>
2026-06-02 12:43:28 +0000

    raphael: Bringup Project Flare
    Signed-off-by: NoCache-69 <chethangowdasj86@gmail.com>

--- flare_raphael.mk
+++ flare_raphael.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common flare stuff.
+$(call inherit-product, vendor/flare/config/common_full_phone.mk)
 
 # Inherit from raphael device
 $(call inherit-product, device/xiaomi/raphael/device.mk)
