c3fe95c529961ee34d7932412324c0911ccb043c
NoCache-69 <chethangowdasj86@gmail.com>
2026-06-02 12:41:36 +0000

    raphael: Update vendorsetup.sh - Track miuicamera from crdroid gitlab - Track ViperFX repo - Track flare branch for trees

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,9 @@
-git clone https://github.com/raphael-resources/android_device_xiaomi_sm8150-common.git device/xiaomi/sm8150-common
+git clone https://github.com/raphael-resources/android_device_xiaomi_sm8150-common.git -b flare device/xiaomi/sm8150-common
 git clone https://github.com/raphael-resources/proprietary_vendor_xiaomi_raphael.git vendor/xiaomi/raphael
 git clone https://github.com/raphael-resources/proprietary_vendor_xiaomi_sm8150-common.git vendor/xiaomi/sm8150-common
-git clone https://github.com/raphael-resources/android_kernel_xiaomi_sm8150.git kernel/xiaomi/sm8150
+git clone https://github.com/raphael-resources/android_kernel_xiaomi_sm8150.git -b flare kernel/xiaomi/sm8150
 git clone https://github.com/raphael-resources/android_hardware_xiaomi.git hardware/xiaomi
-git clone https://github.com/raphael-resources/android_device_xiaomi_miuicamera.git device/xiaomi/miuicamera
-git clone https://github.com/raphael-resources/proprietary_vendor_xiaomi_miuicamera.git vendor/xiaomi/miuicamera
 git clone https://github.com/oscaro-resources/hardware_dolby.git hardware/dolby
+git clone https://github.com/AxionAOSP/android_packages_apps_ViPER4AndroidFX.git -b v4a packages/apps/ViPER4AndroidFX
+git clone https://gitlab.com/crdroidandroid/android_device_xiaomi_miuicamera.git -b 16.0-raphael device/xiaomi/miuicamera
+git clone https://gitlab.com/crdroidandroid/proprietary_vendor_xiaomi_miuicamera.git -b 16.0-raphael vendor/xiaomi/miuicamera
